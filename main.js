4a5829fa9c3dd459efe47a1414c4957a7ac631b9
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-20 07:05:48 -0400

    working on js

---
+++ main.js
@@ -0,0 +1,69 @@
+const keys = document.querySelector('.keys')
+
+let clearkey = document.querySelector('#clearkey')
+console.log(clearkey)
+let numba = document.querySelector('#numba')
+let opto = document.querySelector('#opto')
+let equals = document.querySelector('.equalskey')
+console.log(equals)
+let decimal = document.querySelector('#decimalkey')
+let screen = document.querySelector('.output')
+console.log(numba)
+console.log(equals)
+numba.addEventListener('click')
+opto.addEventListener('click')
+
+/*
+const numberify = document.querySelector(.numberify)
+numberify.addEventListener('click',()=>)
+
+
+const numberButtons = document.querySelector('.buttons button')
+numberButtons.forEach(button=> console.log(button.textContent))
+ console.log above
+
+
+LETS do something besides console.log for button
+(CHANGING THE VALUE of the BUTTONS thru set attributes )
+ numberButtons.forEach(button=> {
+    button.setAttribute('value', 42)
+})
+
+let numberVersion = dictionary[text]
+button.value = numberVersion
+
+google this ^^^^^
+
+
+const sumify = document.querySelector(.'sumify')
+sumify.addEventListener('click', ()=>{})
+
+      We want to add up the value of the BUTTONS
+      Nouns + verbs =
+        value
+        add
+        sum
+numberButtons.forEach(button=> {
+let value =buttons.value
+let sum = 0
+sum += value
+
+
+let numberVersion = 0
+switch (text){
+  case: 'One':
+  number = 1
+  break
+  case: 'Two':
+  number = 2
+  break
+  case: 'Three':
+  number = 3
+  break
+  case: 'Four':
+  number = 4
+  break
+default:
+  numberVersion = 0
+}
+*/
